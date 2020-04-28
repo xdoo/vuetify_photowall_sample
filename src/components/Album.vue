21ee7efeb7f80e1800d3d11b7e08f03e702b9edf
xdoo <claus.straube@gmail.com>
2020-04-28 15:47:39 +0200

    added images

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,5 +1,20 @@
 <template>
-  <v-sheet color="green" width="100%">Hello World</v-sheet>
+  <v-container fluid>
+    <v-row class="display-2 ml-5 mt-9">Flowers, Trees & Bees</v-row>
+    <v-row class="caption font-weight-light my-2 ml-5">16.Apr. 2011 - 26. Apr. 2020</v-row>
+    <v-row dense>
+      <v-col>
+        <v-img></v-img>
+      </v-col>
+      <v-col>
+        <v-img></v-img>
+      </v-col>
+      <v-col>
+        <v-img></v-img>
+      </v-col>
+    </v-row>
+
+  </v-container>
 </template>
 <script lang="ts">
 import Vue from 'vue'
